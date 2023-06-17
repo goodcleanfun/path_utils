1c0ef616db5b74c19514448d5540cfd0578392ee
Al <albarrentine@gmail.com>
2023-06-17 16:40:36 -0400

    [src] adding path functions

---
+++ src/path_utils.c
@@ -0,0 +1,20 @@
+#include "path_utils.h"
+
+bool is_relative_path(struct dirent *ent) {
+    return strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0;
+}
+
+char *path_vjoin(int n, va_list args) {
+    char_array *path = char_array_new();
+    if (path == NULL) return NULL;
+    char_array_add_vjoined(path, PATH_SEPARATOR, true, n, args);
+    return char_array_to_string(path);
+}
+
+char *path_join(int n, ...) {
+    va_list args;
+    va_start(args, n);
+    char *path = path_vjoin(n, args);
+    va_end(args);
+    return path;
+}
---
+++ src/path_utils.h
@@ -0,0 +1,50 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+
+#include "char_array/char_array.h"
+
+#ifdef HAVE_DIRENT_H
+#include <dirent.h>
+#define NAMLEN(dirent) strlen((dirent)->d_name)
+
+#else
+
+#define dirent direct
+#define NAMLEN(dirent) ((dirent)->d_namlen)
+
+#ifdef HAVE_SYS_NDIR_H
+#include <sys/ndir.h>
+#endif
+
+#ifdef HAVE_SYS_DIR_H
+#include <sys/dir.h>
+#endif
+
+#ifdef HAVE_NDIR_H
+#include <ndir.h>
+#endif
+
+#endif
+
+#ifdef _WIN32
+#define PATH_SEPARATOR   "\\"
+#else
+#define PATH_SEPARATOR   "/"
+#endif
+
+#define PATH_SEPARATOR_LEN strlen(PATH_SEPARATOR)
+
+bool is_relative_path(struct dirent *ent);
+
+char *path_join(int n, ...);
+char *path_vjoin(int n, va_list args);
+
+#endif
+
